e1bc87faa59c4b24d10594e0931e436e26c69202
Symone Banks <symonembanks@gmail.com>
2026-03-14 14:56:40 -0400

    Update PalmerPenguins.java

--- edu/fscj/cop2800c/penguin/PalmerPenguins.java
+++ edu/fscj/cop2800c/penguin/PalmerPenguins.java
@@ -1,14 +1,23 @@
 // PalmerPenguins.java
-// D. Singletary
-// 2/22/25
-// Refactored Palmer Penguins analysis using object-oriented approach
+// Symone Banks
+// 3/14/2026
+// Module 9
 
-package edu.fscj.cop2800c.penguin;
 
 public class PalmerPenguins {
-    public static void main(String[] args) {
-        // Create an instance of PenguinAnalyzer
 
+   public static void main(String[] args) {
 
-    }
+      // Create analyzer object
+      PenguinAnalyzer analyzer = new PenguinAnalyzer();
+
+      // Read CSV file
+      analyzer.readPenguinData();
+
+      // Print formatted data
+      analyzer.printPenguinData();
+
+      // Write results to output file
+      analyzer.writePenguinData();
+   }
 }
